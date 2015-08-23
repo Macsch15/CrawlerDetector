efa85597c946d3d55ebc004b025b16792310684a
Maciej Schmidt <macsch15@gmail.com>
2015-08-23 04:38:20 +0200

    Code cleanup

--- src/Detector/CrawlerDetector.php
+++ src/Detector/CrawlerDetector.php
@@ -21,7 +21,7 @@ class CrawlerDetector
         $list = array_merge(require __DIR__ . '/Fixtures/CrawlersList.php');
 
         foreach ($list as $crawler_name => $crawler_regex) {
-            if (preg_match('/(?:^|[^A-Z0-9\_\-])(?:' . str_replace('/', '\/', $crawler_regex) . ')/i', $user_agent, $matches)) {
+            if (preg_match('/(?:^|[^A-Z0-9\_\-])(?:' . str_replace('/', '\/', $crawler_regex) . ')/i', $user_agent)) {
                 $this->name = trim($crawler_name);
 
                 return true;
