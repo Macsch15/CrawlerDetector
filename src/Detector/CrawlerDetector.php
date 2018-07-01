bf8fb25440290467396720ec6db726c9b5ec810b
Maciej Schmidt <macsch15@gmail.com>
2018-07-01 18:15:56 +0000

    Apply fixes from StyleCI

--- src/Detector/CrawlerDetector.php
+++ src/Detector/CrawlerDetector.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace CrawlerDetector\Detector;
 
 class CrawlerDetector
@@ -9,6 +10,7 @@ class CrawlerDetector
      * Detect crawler.
      *
      * @param string $user_agent
+     *
      * @return bool
      */
     public static function isCrawler($user_agent = null): bool
@@ -17,10 +19,10 @@ class CrawlerDetector
             $user_agent = getenv('HTTP_USER_AGENT');
         }
 
-        $list = array_merge(require __DIR__ . '/Fixtures/CrawlersList.php');
+        $list = array_merge(require __DIR__.'/Fixtures/CrawlersList.php');
 
         foreach ($list as $crawler_name => $crawler_regex) {
-            if (preg_match('/(?:^|[^A-Z0-9\_\-])(?:'.str_replace('/', '\/', $crawler_regex) . ')/i', $user_agent)) {
+            if (preg_match('/(?:^|[^A-Z0-9\_\-])(?:'.str_replace('/', '\/', $crawler_regex).')/i', $user_agent)) {
                 self::$name = trim($crawler_name);
 
                 return true;
